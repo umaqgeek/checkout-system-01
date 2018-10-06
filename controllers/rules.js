54fc7c5369f6c474b3b66fa811e286288773aa9c
Umar Mukhtar <umar@tuffah.info>
2018-10-06 10:17:35 +0800

    done all api

--- controllers/rules.js
+++ controllers/rules.js
@@ -1,5 +1,23 @@
 'use strict';
 module.exports = function() {
+  // promotion for unilever.
+  this.unilever = ['Gets a <strong>“3 for 2” deal on Classic Ads</strong>.'];
+  // promotion for apple.
+  this.apple = ['Gets a discount on <strong>Standout Ads where the price '+
+    'drops to $299.99 per ad</strong>.'];
+  // promotion for nike.
+  this.nike = ['Gets a discount on <strong>Premium Ads where 4 or more'+
+    '</strong> are purchased. The price drops to <strong>$379.99 per ad'+
+    '</strong>.'];
+  // promotion for ford.
+  this.ford = [
+    'Gets a <strong>“5 for 4” deal on Classic Ads</strong>.',
+    'Gets a discount on <strong>Standout Ads where the price drops to $309.99'+
+      ' per ad</strong>.',
+    'Gets a discount on <strong>Premium Ads when 3 or more</strong> are '+
+      'purchased. The price drops to <strong>$389.99 per ad</strong>.'
+  ];
+  // pricing rule for unilever.
   this.getUnilever = function (confirmedItems) {
     let total = 0;
     let countClassic = confirmedItems.filter(function (item) {
@@ -23,6 +41,7 @@ module.exports = function() {
     });
     return total;
   };
+  // pricing rule for apple.
   this.getApple = function (confirmedItems) {
     let total = 0;
     confirmedItems.forEach(function (item) {
@@ -31,6 +50,7 @@ module.exports = function() {
     });
     return total;
   };
+  // pricing rule for nike.
   this.getNike = function (confirmedItems) {
     let total = 0;
     let countPremium = confirmedItems.filter(function (item) {
@@ -42,6 +62,7 @@ module.exports = function() {
     });
     return total;
   };
+  // pricing rule for ford.
   this.getFord = function (confirmedItems) {
     let total = 0;
     let countClassic = confirmedItems.filter(function (item) {
@@ -71,6 +92,7 @@ module.exports = function() {
     });
     return total;
   };
+  // pricing rule for default or others.
   this.getDefault = function (confirmedItems) {
     let total = 0;
     confirmedItems.forEach(function (item) {
--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PricingRules = require('./controllers/pricing-rules');
+const Advertisement = require('./models/advertisement-model');
 app.use(bodyParser.json());
+// app start at port 8081.
 app.listen(8081, function () {
   console.log("\x1b[32m");
   console.log(" _____ _             _           _    ____  _  __");
@@ -15,13 +17,32 @@ app.listen(8081, function () {
   console.log("\x1b[0m");
   console.log("Architect app started OK");
 });
+// REST API /calculate.
 app.post('/calculate', function (req, res) {
   console.log('/calculate');
-  let pricingRules = new PricingRules(req.body.customer);
-  let items = req.body.skus;
+  let customer = typeof req.body.customer !== 'undefined' ? req.body.customer :
+    'default';
+  let pricingRules = new PricingRules(customer);
+  let items = typeof req.body.skus !== 'undefined' && req.body.skus.length > 0
+    ? req.body.skus : [];
   items.forEach(function (item) {
     pricingRules.add(item);
   });
   let total = pricingRules.total();
   res.send(total);
+});
+// REST API /advertisement.
+app.get('/advertisement', function (req, res) {
+  console.log('/advertisement');
+  let ads = new Advertisement();
+  let adsId = typeof req.query.id !== 'undefined' ? req.query.id : 'all';
+  let outData;
+  if (adsId !== 'all') {
+    outData = ads.data.filter(function (ad) {
+      return ad.id.toLowerCase() === adsId;
+    });
+  } else {
+    outData = ads.data;
+  }
+  res.send(outData);
 });
